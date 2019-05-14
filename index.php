9657e1387c6100ce77497868617046793ebb6520
Muaz Yediyüzkırkiki <39378729+muaz742@users.noreply.github.com>
2019-05-14 15:15:06 +0300

    element tanımları iyileştirildi

--- index.php
+++ index.php
@@ -19,30 +19,24 @@ $vtkullaniciparola = "";
 $httpHost = "sistem.site"; //alan adı
 $gui['lang']='tr'; //varsayılan dil
 $icerik['mNo'] = '0';
-$icerik['title'] = 'ORMANA HOŞGELDİN';
+$icerik['head']['title'] = 'ORMANA HOŞGELDİN';
 $icerik['baslik'] = 'ÇOK GÜZEL BİR ORMANDASIN';
 $icerik['yazi'] = 'ORMANDA DOLAŞIRKEN<br>KENDİNİ KEŞFEDEBİLECEĞİN BİR MACERAYA<br>HAZIR MISIN?<br>';
-$icerik['btn0Yazi'] = 'ORMANA GİR';
-$icerik['btn0Onclick'] = "secim(".$icerik['mNo'].',0)';
-$icerik['btn0Href'] = '';
-$icerik['btn010yazi'] = 'ORMANDAN ÇIK';
-$icerik['btn010Onclick'] = 'secim(0,10)';
-$icerik['btn010Href'] = '';
-$icerik['btn011yazi'] = 'BAŞA DÖN';
-$icerik['btn011Onclick'] = 'secim(0,11)';
-$icerik['btn011Href'] = '';
-$icerik['btn012yazi'] = 'FİKRİM VAR !';
-$icerik['btn012Onclick'] = 'secim(0,12)';
-$icerik['btn012Href'] = '';
-$icerik['btn013yazi'] = 'PAYLAŞ';
-$icerik['btn013Onclick'] = 'secim(0,13)';
-/*
-$icerik['btn013Onclick'] = "Toast.fire({
-  type: 'error',
-  title: 'fonksiyon pasif'
-})";
-*/
-$icerik['btn013Href'] = '#';
+$icerik['btn0']['yazi'] = 'ORMANA GİR';
+$icerik['btn0']['onclick'] = 'onclick="secim(0,0)"';
+$icerik['btn0']['href'] = ' ';
+$icerik['btn010']['yazi'] = 'ORMANDAN ÇIK';
+$icerik['btn010']['onclick'] = 'onclick="secim(0,10)"';
+$icerik['btn010']['href'] = ' ';
+$icerik['btn011']['yazi'] = 'BAŞA DÖN';
+$icerik['btn011']['onclick'] = 'onclick="secim(0,11)"';
+$icerik['btn011']['href'] = ' ';
+$icerik['btn012']['yazi'] = 'FİKRİM VAR !';
+$icerik['btn012']['onclick'] = 'onclick="secim(0,12)"';
+$icerik['btn012']['href'] = ' ';
+$icerik['btn013']['yazi'] = 'PAYLAŞ';
+$icerik['btn013']['onclick'] = 'onclick="secim(0,13)"';
+$icerik['btn013']['href'] = 'href="#"';
 $logoyazi = 'WALK ON THE JUNGLE';
 $yol = 'view/';
 
@@ -172,7 +166,7 @@ if (!empty($talep[1])){
     if ($talep[1]=="e"&&!empty($talep[2])){
         koddanIcerikGetir($talep['2']);
         $icerik['mNo'] = '30';
-        $icerik['title'] = 'ORMANA HOŞGELDİN';
+        $icerik['head']['title'] = 'ORMANA HOŞGELDİN';
         $icerik['baslik'] = 'ORMANDAN';
         $icerik['yazi'] = $sonuc['veri']['yazi'];
         $yol = '../view/';
@@ -206,7 +200,7 @@ $ekran = ('<!DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
     <link rel="shortcut icon" href="' .$yol. 'assets/images/walking.svg" type="image/x-icon">
     <meta name="description" content="ORMANDA DOLAŞIRKEN KENDİNİ KEŞFEDEBİLECEĞİN BİR MACERAYA HAZIR MISIN?">
-    <title>' .$icerik['title']. '</title>
+    <title>' .$icerik['head']['title']. '</title>
     <link rel="stylesheet" href="' .$yol. 'assets/web/assets/mobirise-icons/mobirise-icons.css">
     <link rel="stylesheet" href="' .$yol. 'assets/tether/tether.min.css">
     <link rel="stylesheet" href="' .$yol. 'assets/soundcloud-plugin/style.css">
@@ -257,18 +251,18 @@ $ekran = ('<!DOCTYPE html>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true">
                 <li class="nav-item">
-                    <a class="nav-link link text-white display-4" onclick="'.$icerik['btn010Onclick'].'" href="'.$icerik['btn010Href'].'" target="_blank">
-                        <span class="mbri-magic-stick mbr-iconfont mbr-iconfont-btn"></span>'.$icerik['btn010yazi'].'</a>
+                    <a class="nav-link link text-white display-4" '.$icerik['btn010']['onclick'].' '.$icerik['btn010']['href'].' target="_blank">
+                        <span class="mbri-magic-stick mbr-iconfont mbr-iconfont-btn"></span>'.$icerik['btn010']['yazi'].'</a>
                 </li>
-                <li class="nav-item"><a class="nav-link link text-white display-4" onclick="'.$icerik['btn011Onclick'].'" href="'.$icerik['btn011Href'].'">
-                    <span class="mbri-home mbr-iconfont mbr-iconfont-btn"></span> '.$icerik['btn011yazi'].'</a></li>
+                <li class="nav-item"><a class="nav-link link text-white display-4" '.$icerik['btn011']['onclick'].' '.$icerik['btn011']['href'].'>
+                    <span class="mbri-home mbr-iconfont mbr-iconfont-btn"></span> '.$icerik['btn011']['yazi'].'</a></li>
                 <li class="nav-item">
-                    <a class="nav-link link text-white display-4" onclick="'.$icerik['btn012Onclick'].'" href="'.$icerik['btn012Href'].'"><span
-                            class="mbri-idea mbr-iconfont mbr-iconfont-btn"></span>'.$icerik['btn012yazi'].'</a>
+                    <a class="nav-link link text-white display-4" '.$icerik['btn012']['onclick'].' '.$icerik['btn012']['href'].'><span
+                            class="mbri-idea mbr-iconfont mbr-iconfont-btn"></span>'.$icerik['btn012']['yazi'].'</a>
                 </li>
             </ul>
-            <div class="navbar-buttons mbr-section-btn"><a class="btn btn-sm btn-white-outline display-4" href="'.$icerik['btn013Href'].'"
-                                                           onclick="'.$icerik['btn013Onclick'].'">'.$icerik['btn013yazi'].'</a></div>
+            <div class="navbar-buttons mbr-section-btn"><a class="btn btn-sm btn-white-outline display-4" '.$icerik['btn013']['href'].'
+                                                           '.$icerik['btn013']['onclick'].'>'.$icerik['btn013']['yazi'].'</a></div>
         </div>
     </nav>
 </section>
@@ -284,7 +278,7 @@ $ekran = ('<!DOCTYPE html>
                 <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1" id="mBaslik">'.$icerik['baslik'].'</h1>
                 <p class="mbr-text pb-3 mbr-fonts-style display-5" id="mYazi">'.$icerik['yazi'].'</p>
                 <div class="mbr-section-btn" id="mButonluk">
-                    <a class="btn btn-md btn-primary display-4 butonamk" id="mBtn0" href="'.$icerik['btn0Href'].'" onclick="'.$icerik['btn0Onclick'].'">'.$icerik['btn0Yazi'].'</a>
+                    <a class="btn btn-md btn-primary display-4 butonamk" id="mBtn0" '.$icerik['btn0']['href'].' '.$icerik['btn0']['onclick'].'>'.$icerik['btn0']['yazi'].'</a>
                 </div>
             </div>
         </div>
