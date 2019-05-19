0d65b56944464aa82095d3addf6748e226fd5225
Muaz Yediyüzkırkiki <39378729+muaz742@users.noreply.github.com>
2019-05-20 00:45:08 +0300

    indentasyon düzenlemesi yapıldı

--- index.php
+++ index.php
@@ -16,15 +16,15 @@ $vtkullaniciisim = "";
 $vtkullaniciparola = "";
 
 /** veritabanı bağlantısı yap */
-try{
+try {
     $vt = new PDO("mysql:host=$sunucu;dbname=$vtisim;charset=utf8", "$vtkullaniciisim", "$vtkullaniciparola");
-}catch (PDOException $e){
+} catch (PDOException $e) {
     print $e->getMessage();
 }
 
 /** yapı bilgilerini tanımla */
 $httpHost = "sistem.site"; //alan adı
-$gui['lang']='tr'; //varsayılan dil
+$gui['lang'] = 'tr'; //varsayılan dil
 
 /** içerik bilgilerini tanımla */
 $icerik['mNo'] = '0';
@@ -47,7 +47,9 @@ $icerik['btn013']['yazi'] = 'PAYLAŞ';
 $icerik['btn013']['onclick'] = 'onclick="secim(0,13)"';
 $icerik['btn013']['href'] = 'href="#"';
 $icerik['header2-1']['attr'] = 'class="cid-r3yvzHG8J6 mbr-fullscreen" data-bg-video="https://www.youtube.com/watch?v=gVrfBvCNCkU"';
-if (isMobile()){$icerik['header2-1']['attr'] = 'class="cid-r7lyGYk8HF mbr-fullscreen"';}
+if (isMobile()) {
+    $icerik['header2-1']['attr'] = 'class="cid-r7lyGYk8HF mbr-fullscreen"';
+}
 $logoyazi = 'WALK ON THE JUNGLE';
 $yol = '';
 
@@ -78,7 +80,8 @@ function mod62_decode($girdi)
     return $cikti;
 }
 
-function isMobile() {
+function isMobile()
+{
     return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
 }
 
@@ -97,6 +100,7 @@ function sonucGetir($ekranNo, $sonucTipi = 0)
     $sorgu = $veri->fetch(PDO::FETCH_ASSOC);
     return $sorgu[$tip];
 }
+
 function sonuclariYaz()
 {
     global $sonuc;
@@ -114,27 +118,28 @@ function sonuclariYaz()
         sonucGetir(17, 1) . "<br>" . $parcaYazi;
 }
 
-function koddanIcerikGetir($girdi){
+function koddanIcerikGetir($girdi)
+{
     $no = mod62_decode($girdi);
     global $vt;
-    $veri = $vt->query("SELECT * FROM secimler WHERE time='".$no."'");
+    $veri = $vt->query("SELECT * FROM secimler WHERE time='" . $no . "'");
     $sorgu = $veri->fetch(PDO::FETCH_ASSOC);
     unset($sorgu['id']);
     unset($sorgu['sessionId']);
     unset($sorgu['time']);
     unset($sorgu['user']);
-    $_SESSION['secimler']=$sorgu;
+    $_SESSION['secimler'] = $sorgu;
     sonuclariYaz();
     //return $cikti;
 }
 
 /** http taleplerini index.php ye topla*/
-$talep = explode('/',$_SERVER['REQUEST_URI']);
+$talep = explode('/', $_SERVER['REQUEST_URI']);
 //dil talebi yokla - yoksa tr
 
 /** varsa sonuç görüntüleme ekranı yazdır */
-if (!empty($talep[1])){
-    if ($talep[1]=="e"&&!empty($talep[2])){
+if (!empty($talep[1])) {
+    if ($talep[1] == "e" && !empty($talep[2])) {
         koddanIcerikGetir($talep['2']);
         $icerik['mNo'] = '30';
         $icerik['head']['title'] = 'ORMANA HOŞGELDİN';
@@ -143,7 +148,7 @@ if (!empty($talep[1])){
         $yol = '../';
         //echo $ekran;
         //exit;
-    }else{
+    } else {
         /** yoksa boş talep ile ana dizine geri göndür */
         header('Location: /');
         exit;
@@ -161,19 +166,19 @@ $ekran = ('<!DOCTYPE html>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="generator" content="Mobirise v4.9.2, mobirise.com">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
-    <link rel="shortcut icon" href="' .$yol. 'assets/images/walking.svg" type="image/x-icon">
+    <link rel="shortcut icon" href="' . $yol . 'assets/images/walking.svg" type="image/x-icon">
     <meta name="description" content="ORMANDA DOLAŞIRKEN KENDİNİ KEŞFEDEBİLECEĞİN BİR MACERAYA HAZIR MISIN?">
-    <title>' .$icerik['head']['title']. '</title>
-    <link rel="stylesheet" href="' .$yol. 'assets/web/assets/mobirise-icons/mobirise-icons.css">
-    <link rel="stylesheet" href="' .$yol. 'assets/tether/tether.min.css">
-    <link rel="stylesheet" href="' .$yol. 'assets/soundcloud-plugin/style.css">
-    <link rel="stylesheet" href="' .$yol. 'assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="' .$yol. 'assets/bootstrap/css/bootstrap-grid.min.css">
-    <link rel="stylesheet" href="' .$yol. 'assets/bootstrap/css/bootstrap-reboot.min.css">
-    <link rel="stylesheet" href="' .$yol. 'assets/dropdown/css/style.css">
-    <link rel="stylesheet" href="' .$yol. 'assets/socicon/css/styles.css">
-    <link rel="stylesheet" href="' .$yol. 'assets/theme/css/style.css">
-    <link rel="stylesheet" href="' .$yol. 'assets/mobirise/css/mbr-additional.css" type="text/css">
+    <title>' . $icerik['head']['title'] . '</title>
+    <link rel="stylesheet" href="' . $yol . 'assets/web/assets/mobirise-icons/mobirise-icons.css">
+    <link rel="stylesheet" href="' . $yol . 'assets/tether/tether.min.css">
+    <link rel="stylesheet" href="' . $yol . 'assets/soundcloud-plugin/style.css">
+    <link rel="stylesheet" href="' . $yol . 'assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="' . $yol . 'assets/bootstrap/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="' . $yol . 'assets/bootstrap/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="' . $yol . 'assets/dropdown/css/style.css">
+    <link rel="stylesheet" href="' . $yol . 'assets/socicon/css/styles.css">
+    <link rel="stylesheet" href="' . $yol . 'assets/theme/css/style.css">
+    <link rel="stylesheet" href="' . $yol . 'assets/mobirise/css/mbr-additional.css" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
 </head>
 <body>
@@ -203,44 +208,44 @@ $ekran = ('<!DOCTYPE html>
             <div class="navbar-brand">
                 <span class="navbar-logo">
                     <a href="#header2-1">
-                         <img src="' .$yol. 'assets/images/walking.svg" alt="WALK ON THE FOREST" title="EXPLORER"
+                         <img src="' . $yol . 'assets/images/walking.svg" alt="WALK ON THE FOREST" title="EXPLORER"
                               style="height: 3.8rem;">
                     </a>
                 </span>
-                <span class="navbar-caption-wrap"><a class="navbar-caption text-white display-4" href="#header2-1">' .$logoyazi. '</a></span>
+                <span class="navbar-caption-wrap"><a class="navbar-caption text-white display-4" href="#header2-1">' . $logoyazi . '</a></span>
             </div>
         </div>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true">
                 <li class="nav-item">
-                    <a class="nav-link link text-white display-4" '.$icerik['btn010']['onclick'].' '.$icerik['btn010']['href'].' target="_blank">
-                        <span class="mbri-magic-stick mbr-iconfont mbr-iconfont-btn"></span>'.$icerik['btn010']['yazi'].'</a>
+                    <a class="nav-link link text-white display-4" ' . $icerik['btn010']['onclick'] . ' ' . $icerik['btn010']['href'] . ' target="_blank">
+                        <span class="mbri-magic-stick mbr-iconfont mbr-iconfont-btn"></span>' . $icerik['btn010']['yazi'] . '</a>
                 </li>
-                <li class="nav-item"><a class="nav-link link text-white display-4" '.$icerik['btn011']['onclick'].' '.$icerik['btn011']['href'].'>
-                    <span class="mbri-home mbr-iconfont mbr-iconfont-btn"></span> '.$icerik['btn011']['yazi'].'</a></li>
+                <li class="nav-item"><a class="nav-link link text-white display-4" ' . $icerik['btn011']['onclick'] . ' ' . $icerik['btn011']['href'] . '>
+                    <span class="mbri-home mbr-iconfont mbr-iconfont-btn"></span> ' . $icerik['btn011']['yazi'] . '</a></li>
                 <li class="nav-item">
-                    <a class="nav-link link text-white display-4" '.$icerik['btn012']['onclick'].' '.$icerik['btn012']['href'].'><span
-                            class="mbri-idea mbr-iconfont mbr-iconfont-btn"></span>'.$icerik['btn012']['yazi'].'</a>
+                    <a class="nav-link link text-white display-4" ' . $icerik['btn012']['onclick'] . ' ' . $icerik['btn012']['href'] . '><span
+                            class="mbri-idea mbr-iconfont mbr-iconfont-btn"></span>' . $icerik['btn012']['yazi'] . '</a>
                 </li>
             </ul>
-            <div class="navbar-buttons mbr-section-btn"><a class="btn btn-sm btn-white-outline display-4" '.$icerik['btn013']['href'].'
-                                                           '.$icerik['btn013']['onclick'].'>'.$icerik['btn013']['yazi'].'</a></div>
+            <div class="navbar-buttons mbr-section-btn"><a class="btn btn-sm btn-white-outline display-4" ' . $icerik['btn013']['href'] . '
+                                                           ' . $icerik['btn013']['onclick'] . '>' . $icerik['btn013']['yazi'] . '</a></div>
         </div>
     </nav>
 </section>
 
 <section class="engine"><a href="https://mobirise.info/r">bootstrap template</a></section>
-<section '.$icerik['header2-1']['attr'].'
+<section ' . $icerik['header2-1']['attr'] . '
          id="header2-1">
     <div class="mbr-overlay" style="opacity: 0.5; background-color: rgb(118, 118, 118);"></div>
     <div class="container align-center">
         <div class="row justify-content-md-center">
             <div class="mbr-white col-md-10" id="mEkran">
-                <div id="mNo" hidden>'.$icerik['mNo'].'</div>
-                <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1" id="mBaslik">'.$icerik['baslik'].'</h1>
-                <p class="mbr-text pb-3 mbr-fonts-style display-5" id="mYazi">'.$icerik['yazi'].'</p>
+                <div id="mNo" hidden>' . $icerik['mNo'] . '</div>
+                <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1" id="mBaslik">' . $icerik['baslik'] . '</h1>
+                <p class="mbr-text pb-3 mbr-fonts-style display-5" id="mYazi">' . $icerik['yazi'] . '</p>
                 <div class="mbr-section-btn" id="mButonluk">
-                    <a class="btn btn-md btn-primary display-4 butonamk" id="mBtn0" '.$icerik['btn0']['href'].' '.$icerik['btn0']['onclick'].'>'.$icerik['btn0']['yazi'].'</a>
+                    <a class="btn btn-md btn-primary display-4 butonamk" id="mBtn0" ' . $icerik['btn0']['href'] . ' ' . $icerik['btn0']['onclick'] . '>' . $icerik['btn0']['yazi'] . '</a>
                 </div>
             </div>
         </div>
@@ -250,33 +255,33 @@ $ekran = ('<!DOCTYPE html>
     <div class="container">
         <div class="media-container-row align-center mbr-white">
             <div class="row row-copirayt">
-                <p class="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7">© COPYRIGHT ' .date(Y). ' <a
+                <p class="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7">© COPYRIGHT ' . date(Y) . ' <a
                         href="http://www.muaz712.com" class="text-white" target="_blank">MUAZ712.COM</a>- TÜM HAKLARI
                     SAKLIDIR</p>
             </div>
             <div class="row row-copirayt">
                 <p class="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7">
                 <a href="https://github.com/muaz742/cok-guzel-bir-orman" target="_blank">
-                <img src="' .$yol. 'assets/images/github.svg" alt="" style="height: 2.0rem;">
+                <img src="' . $yol . 'assets/images/github.svg" alt="" style="height: 2.0rem;">
                 </a>
                 </p>
             </div>
         </div>
     </div>
 </section>
-<script src="' .$yol. 'assets/web/assets/jquery/jquery.min.js"></script>
-<script src="' .$yol. 'assets/popper/popper.min.js"></script>
-<script src="' .$yol. 'assets/tether/tether.min.js"></script>
-<script src="' .$yol. 'assets/bootstrap/js/bootstrap.min.js"></script>
-<script src="' .$yol. 'assets/smoothscroll/smooth-scroll.js"></script>
-<script src="' .$yol. 'assets/dropdown/js/script.min.js"></script>
-<script src="' .$yol. 'assets/touchswipe/jquery.touch-swipe.min.js"></script>
-<script src="' .$yol. 'assets/ytplayer/jquery.mb.ytplayer.min.js"></script>
-<script src="' .$yol. 'assets/vimeoplayer/jquery.mb.vimeo_player.js"></script>
-<script src="' .$yol. 'assets/theme/js/script.js"></script>
+<script src="' . $yol . 'assets/web/assets/jquery/jquery.min.js"></script>
+<script src="' . $yol . 'assets/popper/popper.min.js"></script>
+<script src="' . $yol . 'assets/tether/tether.min.js"></script>
+<script src="' . $yol . 'assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="' . $yol . 'assets/smoothscroll/smooth-scroll.js"></script>
+<script src="' . $yol . 'assets/dropdown/js/script.min.js"></script>
+<script src="' . $yol . 'assets/touchswipe/jquery.touch-swipe.min.js"></script>
+<script src="' . $yol . 'assets/ytplayer/jquery.mb.ytplayer.min.js"></script>
+<script src="' . $yol . 'assets/vimeoplayer/jquery.mb.vimeo_player.js"></script>
+<script src="' . $yol . 'assets/theme/js/script.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
 
-<script src="http://orman.muaz712.com/index.js?v=' .time(). '"></script>
+<script src="http://orman.muaz712.com/index.js?v=' . time() . '"></script>
 </body>
 </html>');
 
