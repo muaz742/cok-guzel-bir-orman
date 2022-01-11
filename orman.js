3c50db5b04ad0376f50343e6fe3fc4782f896468
Muaz Yediyüzkırkiki <39378729+muaz742@users.noreply.github.com>
2022-01-11 20:52:18 +0300

    Orman sınıfı oluşturuldu.

--- orman.js
+++ orman.js
@@ -1,155 +1,98 @@
-function getJsonToObj(pathJson) {
-    /** get JSON file */
-    var getJson = new XMLHttpRequest();
-    getJson.open("GET", pathJson, false);
-    getJson.send();
-    var json = getJson.response;
-
-    /** JSON to Object */
-    var objJson = JSON.parse(json);
-
-    /** return Object */
-    return objJson;
-}
-
-/** konfigürasyon */
-var pathJson = "/docs/iceriklik.json";
-var secimler = [];
-
-/** içerik çek */
-var icerik = getJsonToObj(pathJson);
-
-function sec(hedef) {
-    /** ekran ve seçim numarasını algıla */
-    var ekranNo = algilaNo("ekran", hedef);
-    var secimNo = algilaNo("secim", hedef);
-    console.log(ekranNo + "-" + secimNo);
-
-    /** seçimi oturum kaydına kaydet */
-    secimler[ekranNo] = secimNo;
-
-    /** aksiyon algıla */
-    var aksiyon = algilaAksiyon(icerik.ekran[ekranNo].secenekler[secimNo].aksiyon);
-    var aksiyonSayisi = Object.keys(aksiyon).length;
-    console.log(aksiyon);
-
-    /** aksiyonları işle */
-    for (var i = 0; i < aksiyonSayisi; i++) {
-        isleAksiyon(aksiyon[i].tip, aksiyon[i].veri);
+class Orman {
+    constructor() {
+        this.ekranNo = 1;
+        this.secimler = {};
     }
-}
-
-function algilaNo(id, element) {
-    switch (id) {
-        case "ekran":
-            return element.parentElement.parentElement.getAttribute("data-id");
-            break;
-        case "secim":
-            return element.getAttribute("data-id");
-            break;
-        default:
-            return 5;
+    icerikEkle(pathJson) {
+        /** get JSON file */
+        var getJson = new XMLHttpRequest();
+        getJson.open("GET", pathJson, false);
+        getJson.send();
+        var json = getJson.response;
+        /** JSON to Object */
+        this.icerik = JSON.parse(json);
     }
-}
-
-function algilaAksiyon(aksiyon) {
-    if (aksiyon) {
-        return aksiyon;
-    } else {
-        return {0: {tip: 0, veri: "aksiyon yok from 'algilaAksiyon();'"}};
+    _butonlariSil() {
+        var butonluk = document.querySelector('#mButonluk')
+        while (butonluk.firstChild) {
+            butonluk.removeChild(butonluk.firstChild);
+        }
     }
-}
+    _butonEkle(yazi, id, onclick = "orman.sec(" + id + ")") {
+        var buton = document.createElement('a');
+        buton.className = "btn btn-md btn-primary display-4";
+        buton.id = "mBtn" + id;
+        buton.innerHTML = yazi;
+        buton.setAttribute("onclick", onclick);
+        document.querySelector('#mButonluk').appendChild(buton);
+    }
+    /* TODO
+    _arayuzYukle(){
+        var arayuz = this.icerik.arayuz
+        document.querySelector('#guiBaslik').innerHTML = arayuz.baslik;
+        document.querySelector('#guiLogo').src = arayuz.logo;
+    }
+    */
+    _sonucYazdir(ekranlar = [], tip = 0) {
+        var yazi = ""
+        var ekran = this.icerik.ekran
+        for (let i = 0; i < ekranlar.length; i++) {
+            console.log(ekranlar[i]);
+            var ekranNo = ekranlar[i]
+            var secimNo = this.secimler[ekranlar[i]]
+            try {
+                yazi += " " + ekran[ekranNo].secenekler[secimNo].sonuc[tip]
+            } catch {
+                console.log(ekranlar[i] + " ekranında seçim yok.");
+            }
+        }
+        return yazi
+    }
+    _ekranYukle() {
+        var ekran =
+            this.icerik.ekran[this.ekranNo]
+        // başlık güncelle
+        document.querySelector('#mBaslik').innerHTML = ekran.baslik;
+        // yazı güncelle
+        if (ekran.yazi.sonuc) {
+            console.log(ekran.yazi.sonuc);
+            var sonuc = this._sonucYazdir(ekran.yazi.sonuc, 1)
+            document.querySelector('#mYazi').innerHTML = sonuc
+        } else {
+            document.querySelector('#mYazi').innerHTML = ekran.yazi;
+            console.log("sonuc yok");
+        }
+        // buton güncelle
+        this._butonlariSil();
+        for (const [key, value] of Object.entries(ekran.secenekler)) {
+            value.onclick ? this._butonEkle(value.yazi, key, value.onclick) : this._butonEkle(value.yazi, key);
+        }
+    }
+    sec(id) {
+        // seçimi kaydet
+        this.secimler[this.ekranNo] = id;
+        console.log(this.secimler);
 
-function isleAksiyon(tip, veri) {
-    switch (tip) {
-        case 0: //aksiyon yok
-            console.log("aksiyon yok from 'isleAksiyon();'");
-            break;
-        case 1: //yönlendir - yeni sekmede
-            (veri.mesaj) ? alert(veri.mesaj) : null;
-            (veri.url) ? window.open(veri.url) : console.log("url nof found");
-            break;
-        case 2: //yönlendir - ekranda
-            (veri.mesaj) ? alert(veri.mesaj) : null;
-            (veri.url) ? window.open(veri.url, "_parent") : console.log("url nof found");
-            break;
-        case 3: //ekran güncelle
-            // yazıları güncelle
-            mEkranYaz(veri);
-            break;
-        case 4: //alert
-            (veri.mesaj) ? alert(veri.mesaj) : null;
-            break;
-        case 5: //sw alert
-            (veri.baslik) ? null : veri.baslik = "Hata";
-            (veri.url) ? null : veri.url = "Hata";
-            Swal.fire({
-                title: veri.baslik,
-                input: 'text',
-                inputValue: veri.url,
-                showCancelButton: false,
-                showConfirmButton: true,
-                confirmButtonText: 'KOPYALA',
-                confirmButtonColor: '#6c757d',
-                animation: false,
-                customClass: {
-                    popup: 'animated tada'
+        // ekranı güncelle
+        var buton = this.icerik.ekran[this.ekranNo].secenekler[id];
+        if (buton.zipla) {
+            if (buton.zipla.kosul) {
+                if (this.secimler[buton.zipla.kosul.ekran] == buton.zipla.kosul.secim) {
+                    this.ekranNo = buton.zipla.hedef;
+                } else {
+                    this.ekranNo++;
                 }
-            }).then((result) => {
-                if (result.value) {
-                    copyToClipboard(result.value);
-                    Toast.fire({
-                        type: 'success',
-                        title: 'Kopyalandı 👍'
-                    })
-                }
-            });
-            break;
-        case 6: //sw toast
-            (veri.tip) ? null : veri.tip = "error";
-            (veri.mesaj) ? null : veri.mesaj = "Hata";
-            Toast.fire({
-                type: veri.tip,
-                title: veri.mesaj,
-            });
-            break;
-        default:
-            console.log("aksiyon algılanamadı");
+            } else {
+                this.ekranNo = buton.zipla.hedef;
+            }
+        } else {
+            this.ekranNo++;
+        }
+        //this.acilisIslemleri()
+        this._ekranYukle();
     }
-    (veri.log) ? console.log(veri.log) : null;
-}
-
-/** panoya kopyala fonkisyonu tanımla */
-/** https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f */
-const copyToClipboard = str => {
-    const el = document.createElement('textarea');  // Create a <textarea> element
-    el.value = str;                                 // Set its value to the string that you want copied
-    el.setAttribute('readonly', '');                // Make it readonly to be tamper-proof
-    el.style.position = 'absolute';
-    el.style.left = '-9999px';                      // Move outside the screen to make it invisible
-    document.body.appendChild(el);                  // Append the <textarea> element to the HTML document
-    const selected =
-        document.getSelection().rangeCount > 0        // Check if there is any content selected previously
-            ? document.getSelection().getRangeAt(0)     // Store selection if found
-            : false;                                    // Mark as false to know no selection existed before
-    el.select();                                    // Select the <textarea> content
-    document.execCommand('copy');                   // Copy - only works as a result of a user action (e.g. click events)
-    document.body.removeChild(el);                  // Remove the <textarea> element
-    if (selected) {                                 // If a selection existed before copying
-        document.getSelection().removeAllRanges();    // Unselect everything on the HTML document
-        document.getSelection().addRange(selected);   // Restore the original selection
+    calistir() {
+        //this._arayuzYukle();
+        this._ekranYukle();
     }
 }
-
-/** tost gösterimi tanımla */
-const Toast = Swal.mixin({
-    toast: true,
-    position: 'top-end',
-    showConfirmButton: false,
-    timer: 3000,
-    timerProgressBar: true,
-    onOpen: (toast) => {
-        toast.addEventListener('mouseenter', Swal.stopTimer)
-        toast.addEventListener('mouseleave', Swal.resumeTimer)
-    }
-})
